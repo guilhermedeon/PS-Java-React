252dbbd006e1c577814078f5f950b607ff88607a
Guilherme Deon de Faria <guibyn@gmail.com>
2023-07-16 17:55:40 -0300

    Fixed "id" name; Added Json annotations for Jackson to prevent loop Added "saldo" and "postLoad"

--- src/main/java/br/com/banco/entities/Conta.java
+++ src/main/java/br/com/banco/entities/Conta.java
@@ -1,10 +1,10 @@
 package br.com.banco.entities;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +13,25 @@ import java.util.List;
 @Getter
 @Setter
 @EqualsAndHashCode (of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Conta {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id_conta;
     private String nome_responsavel;
-    @OneToMany (mappedBy = "conta")
+    @OneToMany(mappedBy = "conta")
+    @JsonManagedReference
     private List<Transferencia> transferencias = new ArrayList<>();
+    @Transient
+    private BigDecimal saldo;
+
+    @PostLoad
+    public void postLoad() {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Transferencia tr : transferencias){
+            sum = sum.add(tr.getValor());
+        }
+        this.saldo = sum;
+    }
 }
