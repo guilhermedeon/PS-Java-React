b31eb98c4adad51ea99a4c94379452fd3c6fc1de
Guilherme Deon de Faria <guibyn@gmail.com>
2023-07-16 22:05:55 -0300

    New Class TransferenciaService

---
+++ src/main/java/br/com/banco/services/TransferenciaService.java
@@ -0,0 +1,15 @@
+package br.com.banco.services;
+
+import br.com.banco.repositories.TransferenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransferenciaService {
+
+    @Autowired
+    TransferenciaRepository repo;
+
+
+
+}
