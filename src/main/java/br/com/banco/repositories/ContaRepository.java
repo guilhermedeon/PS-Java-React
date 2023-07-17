28aa6fb2d034b9f79b1e6f5d42373a58f2037bf3
Guilherme Deon de Faria <guibyn@gmail.com>
2023-07-16 22:05:37 -0300

    New Class ContaService + functionalities

--- src/main/java/br/com/banco/repositories/ContaRepository.java
+++ src/main/java/br/com/banco/repositories/ContaRepository.java
@@ -3,5 +3,8 @@ package br.com.banco.repositories;
 import br.com.banco.entities.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContaRepository extends JpaRepository<Conta, Long> {
+    public Optional<Conta> findByNomeResponsavel(String nomeResponsavel);
 }
---
+++ src/main/java/br/com/banco/services/ContaService.java
@@ -0,0 +1,46 @@
+package br.com.banco.services;
+
+import br.com.banco.entities.Conta;
+import br.com.banco.entities.DTO.ContaGetDTO;
+import br.com.banco.entities.DTO.ContaPostDTO;
+import br.com.banco.repositories.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ContaService {
+
+    @Autowired
+    ContaRepository repo;
+
+    public void add(ContaPostDTO data){
+        if (repo.findByNomeResponsavel(data.getNomeResponsavel()).isPresent()){
+            throw new RuntimeException("Responsável já possui conta");
+        }
+        Conta c = new Conta(data);
+        repo.save(c);
+    }
+
+    public List<ContaGetDTO> getAllContas(){
+        return repo.findAll().stream().map(ContaGetDTO::new).collect(Collectors.toList());
+    }
+
+    public Optional<Conta> getByName(String name){
+        return repo.findByNomeResponsavel(name);
+    }
+
+    public Optional<Conta> getById (Long id){
+        return Optional.of(repo.getById(id));
+    }
+
+    public void deleteById(Long id){
+        repo.deleteById(id);
+    }
+
+
+}
