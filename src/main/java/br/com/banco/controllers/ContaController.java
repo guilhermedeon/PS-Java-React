a1f06bda4c3e6c494f55e513e5d7f1cbfaffd6f1
Guilherme Deon de Faria <guibyn@gmail.com>
2023-07-17 21:39:17 -0300

    Renamed MainController to ContaController

--- src/main/java/br/com/banco/controllers/ContaController.java
+++ src/main/java/br/com/banco/controllers/ContaController.java
@@ -1,29 +1,26 @@
 package br.com.banco.controllers;
 
+import br.com.banco.entities.DTO.ContaGetDTO;
 import br.com.banco.entities.DTO.ContaPostDTO;
 import br.com.banco.services.ContaService;
 import br.com.banco.services.TransferenciaService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
-public class MainController {
+public class ContaController {
 
     @Autowired
     ContaService contaService;
-    @Autowired
-    TransferenciaService transferenciaService;
 
     @PostMapping("conta/add/")
     public ResponseEntity novaConta(@RequestBody ContaPostDTO data){
         contaService.add(data);
-        return ResponseEntity.ok(contaService.getByName(data.getNomeResponsavel()));
+        return ResponseEntity.ok(new ContaGetDTO(contaService.getByName(data.getNomeResponsavel()).get()));
     }
 
     @DeleteMapping("conta/delete/")
