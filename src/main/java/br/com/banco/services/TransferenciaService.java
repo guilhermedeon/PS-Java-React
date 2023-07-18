f2e69c410c72ffc3f69360b4fbb701aa0a938e7b
Guilherme Deon de Faria <guibyn@gmail.com>
2023-07-17 21:40:50 -0300

    Implemented get methods for transferencias

--- src/main/java/br/com/banco/services/TransferenciaService.java
+++ src/main/java/br/com/banco/services/TransferenciaService.java
@@ -1,8 +1,19 @@
 package br.com.banco.services;
 
+import br.com.banco.entities.DTO.TransferenciaGetDTO;
+import br.com.banco.entities.DTO.TransferenciaPostDTO;
+import br.com.banco.entities.Transferencia;
+import br.com.banco.repositories.ContaRepository;
 import br.com.banco.repositories.TransferenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TransferenciaService {
@@ -10,6 +21,19 @@ public class TransferenciaService {
     @Autowired
     TransferenciaRepository repo;
 
+    @Autowired
+    ContaRepository contaRepository;
 
+    public List<TransferenciaGetDTO> getAll() {
+        return repo.findAll().stream().map(TransferenciaGetDTO::new).collect(Collectors.toList());
+    }
+
+    public List<TransferenciaGetDTO> getAllPaged(Pageable pageable){
+        return repo.findAll(pageable).stream().map(TransferenciaGetDTO::new).collect(Collectors.toList());
+    }
+
+    public Optional<TransferenciaGetDTO> getById(Long id) {
+        return Optional.of(new TransferenciaGetDTO(repo.findById(id).get()));
+    }
 
 }
